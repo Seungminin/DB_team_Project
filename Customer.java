e197e7138bc5db38d17ae962617220cc1202949b
Kang Seung Min <empe8339@gmail.com>
2023-11-30 15:56:10 +0900

    DB구축

--- Customer.java
+++ Customer.java
@@ -2,122 +2,118 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.sql.Statement;
 
 public class Customer {
-    public static void main(String[] args) {
-        createUserTable();
-        createFollowTable(); // follow 테이블 생성 추가
-        createPostTable(); // post 테이블 생성 추가
-        createCommentTable(); // comment 테이블 생성 추가
-    }
+	public static void main(String[] args) {
+		  try (Connection con = getConnection()) {
+		    createUserTable(); // Create the 'customer' table first
+		    createFollowTable(); // Create the 'follow' table
+		    createPostTable(); // Create the 'post' table
+		    createCommentTable(); // Create the 'comment' table
+		  } catch (Exception e) {
+		    e.printStackTrace();
+		  }
+		}
 
     // 사용을 위해서 해당 프로젝트에 mysqldbconnector 생성 필요
     // SQL 문법에 맞추어 입력되는 값들을 DB로 전송한다
 
-    // Table 생성
-    public static void createUserTable() {
-        try {
-            Connection con = getConnection();
-            PreparedStatement create = con.prepareStatement(
-                    "CREATE TABLE IF NOT EXISTS "
-                            + "customer(id int NOT NULL AUTO_INCREMENT,"
-                            + "name varChar(255),"
-                            + "author varChar(255),"
-                            + "booknumber varChar(255),"
-                            + "publisher varChar(255),"
-                            + "category varChar(255),"
-                            + "introduction varChar(255),"
-                            + "PRIMARY KEY(id))");
+    //Table 생성, DB생성.
+	public static void createUserTable() {
+        try (Connection con = getConnection();
+             PreparedStatement create = con.prepareStatement(
+                     "CREATE TABLE IF NOT EXISTS `db_final`.`customer` (" +
+                             "`user_id` int NOT NULL AUTO_INCREMENT," +
+                             "`password` VARCHAR(255)," +
+                             "`name` VARCHAR(255)," +
+                             "`phone` VARCHAR(255)," +
+                             "`Email` VARCHAR(255)," +
+                             "`birth` VARCHAR(255)," +
+                             "`website` VARCHAR(255)," +
+                             "`gender` VARCHAR(255)," +
+                             "`introduction` VARCHAR(255)," +
+                             "PRIMARY KEY(`user_id`))"+
+                             "Engine = InnoDB;")) {
             create.execute();
+            System.out.println("User table successfully created");
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("Table successfully created");
+            System.out.println("Error creating user table: " + e.getMessage());
         }
     }
 
-    // DB 만든 부분
-    public static void createFollowTable() {
-        try (Connection con = getConnection();
-             PreparedStatement create = con.prepareStatement(
-                     "CREATE TABLE IF NOT EXISTS `modelDB`.`follow` ("
-                             + "`follow_id` VARCHAR(45) NOT NULL,"
-                             + "`follower_id` VARCHAR(45) NOT NULL,"
-                             + "`following_id` VARCHAR(45) NOT NULL,"
-                             + "PRIMARY KEY (`follow_id`),"
-                             + "INDEX `fk_follow_user1_idx` (`follower_id` ASC) VISIBLE,"
-                             + "INDEX `fk_follow_user2_idx` (`following_id` ASC) VISIBLE,"
-                             + "CONSTRAINT `fk_follow_user1`"
-                             + "  FOREIGN KEY (`follower_id`)"
-                             + "  REFERENCES `modelDB`.`user` (`user_id`)"
-                             + "  ON DELETE NO ACTION"
-                             + "  ON UPDATE NO ACTION,"
-                             + "CONSTRAINT `fk_follow_user2`"
-                             + "  FOREIGN KEY (`following_id`)"
-                             + "  REFERENCES `modelDB`.`user` (`user_id`)"
-                             + "  ON DELETE NO ACTION"
-                             + "  ON UPDATE NO ACTION)"
-                             + "ENGINE = InnoDB;")) {
-            create.execute();
-            System.out.println("Follow table successfully created");
-        } catch (Exception e) {
-            System.out.println("Error creating follow table: " + e.getMessage());
-        }
-    }
+	public static void createFollowTable() {
+	    try (Connection con = getConnection();
+	         PreparedStatement create = con.prepareStatement(
+	             "CREATE TABLE IF NOT EXISTS `db_final`.`follow` (" +
+	                     "`follow_id` int NOT NULL AUTO_INCREMENT," +
+	                     "`follower_id` int NOT NULL," +
+	                     "`following_id` int NOT NULL," +
+	                     "PRIMARY KEY (`follow_id`)," +
+	                     "INDEX idx_follower_id (`follower_id`)," +
+	                     "INDEX idx_following_id (`following_id`)," +
+	                     "FOREIGN KEY (`follower_id`) REFERENCES `customer` (`user_id`)" +
+	                     "  ON DELETE NO ACTION ON UPDATE NO ACTION," +
+	                     "FOREIGN KEY (`following_id`) REFERENCES `customer` (`user_id`)" +
+	                     "  ON DELETE NO ACTION ON UPDATE NO ACTION);")) {
 
-    public static void createPostTable() {
-        try (Connection con = getConnection();
-             PreparedStatement create = con.prepareStatement(
-                     "CREATE TABLE IF NOT EXISTS `modelDB`.`post` ("
-                             + "`post_id` VARCHAR(45) NOT NULL,"
-                             + "`content` TEXT NULL,"
-                             + "`location` VARCHAR(30) NULL,"
-                             + "`user_id` VARCHAR(45) NOT NULL,"
-                             + "PRIMARY KEY (`post_id`),"
-                             + "INDEX `fk_post_user1_idx` (`user_id` ASC) VISIBLE,"
-                             + "CONSTRAINT `fk_post_user1`"
-                             + "  FOREIGN KEY (`user_id`)"
-                             + "  REFERENCES `modelDB`.`user` (`user_id`)"
-                             + "  ON DELETE NO ACTION"
-                             + "  ON UPDATE NO ACTION)"
-                             + "ENGINE = InnoDB;")) {
-            create.execute();
-            System.out.println("Post table successfully created");
-        } catch (Exception e) {
-            System.out.println("Error creating post table: " + e.getMessage());
-        }
-    }
+	        create.execute();
+	        System.out.println("Follow table successfully created");
+	    } catch (Exception e) {
+	        System.out.println("Error creating follow table: " + e.getMessage());
+	    }
+	}
 
-    public static void createCommentTable() {
-        try (Connection con = getConnection();
-             PreparedStatement create = con.prepareStatement(
-                     "CREATE TABLE IF NOT EXISTS `modelDB`.`comment` ("
-                             + "`comment_id` VARCHAR(45) NOT NULL,"
-                             + "`content` VARCHAR(200) NULL,"
-                             + "`parent_id` VARCHAR(45) NULL,"
-                             + "`user_id` VARCHAR(45) NOT NULL,"
-                             + "`post_id` VARCHAR(45) NOT NULL,"
-                             + "PRIMARY KEY (`comment_id`),"
-                             + "INDEX `fk_comment_user1_idx` (`user_id` ASC) VISIBLE,"
-                             + "INDEX `fk_comment_post1_idx` (`post_id` ASC) VISIBLE,"
-                             + "CONSTRAINT `fk_comment_user1`"
-                             + "  FOREIGN KEY (`user_id`)"
-                             + "  REFERENCES `modelDB`.`user` (`user_id`)"
-                             + "  ON DELETE NO ACTION"
-                             + "  ON UPDATE NO ACTION,"
-                             + "CONSTRAINT `fk_comment_post1`"
-                             + "  FOREIGN KEY (`post_id`)"
-                             + "  REFERENCES `modelDB`.`post` (`post_id`)"
-                             + "  ON DELETE NO ACTION"
-                             + "  ON UPDATE NO ACTION)"
-                             + "ENGINE = InnoDB;")) {
-            create.execute();
-            System.out.println("Comment table successfully created");
-        } catch (Exception e) {
-            System.out.println("Error creating comment table: " + e.getMessage());
-        }
-    }
+	    public static void createPostTable() {
+	        try (Connection con = getConnection();
+	             PreparedStatement create = con.prepareStatement(
+	                     "CREATE TABLE IF NOT EXISTS `db_final`.`post` (" +
+	                             "`post_id` VARCHAR(45) NOT NULL," +
+	                             "`content` TEXT NULL," +
+	                             "`location` VARCHAR(30) NULL," +
+	                             "`user_id` int NOT NULL," + // 수정: user_id 데이터 타입 변경
+	                             "PRIMARY KEY (`post_id`)," +
+	                             "FOREIGN KEY (`user_id`)" +
+	                             "  REFERENCES `db_final`.`customer` (`user_id`)" +
+	                             "  ON DELETE CASCADE" +
+	                             "  ON UPDATE CASCADE)" +
+	                             "ENGINE = InnoDB;")) {
+	            create.execute();
+	            System.out.println("Post table successfully created");
+	        } catch (Exception e) {
+	            System.out.println("Error creating post table: " + e.getMessage());
+	        }
+	    }
+
+	    public static void createCommentTable() {
+	        try (Connection con = getConnection();
+	             PreparedStatement create = con.prepareStatement(
+	                     "CREATE TABLE IF NOT EXISTS `db_final`.`comment` (" +
+	                             "`comment_id` VARCHAR(45) NOT NULL," +
+	                             "`content` VARCHAR(200) NULL," +
+	                             "`parent_id` VARCHAR(45) NOT NULL," + // Change data type to VARCHAR(45)
+	                             "`user_id` int NOT NULL," +
+	                             "`post_id` VARCHAR(45) NOT NULL," +
+	                             "PRIMARY KEY (`comment_id`)," +
+	                             "FOREIGN KEY (`parent_id`)" +
+	                             "  REFERENCES `db_final`.`comment` (`comment_id`)" + // Change reference table and column
+	                             "  ON DELETE CASCADE" +
+	                             "  ON UPDATE CASCADE," +
+	                             "FOREIGN KEY (`user_id`)" +
+	                             "  REFERENCES `db_final`.`customer` (`user_id`)" +
+	                             "  ON DELETE CASCADE" +
+	                             "  ON UPDATE CASCADE," +
+	                             "FOREIGN KEY (`post_id`)" +
+	                             "  REFERENCES `db_final`.`post` (`post_id`)" +
+	                             "  ON DELETE CASCADE" +
+	                             "  ON UPDATE CASCADE)" +
+	                             "ENGINE = InnoDB;")) {
+	            create.execute();
+	            System.out.println("Comment table successfully created");
+	        } catch (Exception e) {
+	            System.out.println("Error creating comment table: " + e.getMessage());
+	        }
+	    }
 
     // MYSQL 연결
     public static Connection getConnection() {
